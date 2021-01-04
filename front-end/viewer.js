375a72152b3cfbb38c599d081b7858303c320008
Dillonzer <dillonbast@gmail.com>
2021-01-04 16:07:57 -0600

    some small work on it

--- front-end/viewer.js
+++ front-end/viewer.js
@@ -34,12 +34,19 @@ function configureExtension() {
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
+}
+
+function openAbout() 
+{
+  document.getElementById("about").style.display = "";
+  document.getElementById("currentStandings").style.display = "none";
+  document.getElementById("currentMatchInformation").style.display = "none";
+  document.getElementById("streamersMatches").style.display = "none";
+  
 }
